35bf23e88d0ad045c3872f8a8f184555cec93d50
Victoria Lojek <victoria.lojek@web.de>
2022-07-05 18:47:06 +0200

    Add country common name list

--- index.js
+++ index.js
@@ -3,15 +3,27 @@ const app = express()
 const port = 3000
 const axios = require("axios")
 
-app.get("/language/:languageName", async (req, res) => {
+app.get("/language/:language", async (req, res) => {
   try {
-    let languageName = req.params.languageName
-    const response = await axios.get("https://restcountries.com/v3.1/lang/" + languageName)
-    const countries = response.data
+    let language = req.params.language
+    const response = await axios.get("https://restcountries.com/v3.1/lang/" + language)
+    languageCountries = response.data
 
-    languageName = response.data[0].region
+    // given a language, return a list of the common name of all countries using this endpoint
 
-    res.send(`Test Output: <br><br> ${countries}`)
+    let count = 0
+    let countryNames = ""
+    for (i = 0; i < languageCountries.length; i++) {
+      count += 1
+      countryNames += languageCountries[i].name.common + "<br>"
+    }
+    console.log(countryNames)
+
+    // find total population that speaks this language
+
+    // find smallest country which speaks this language
+
+    res.send(`Output: <br><br> ${countryNames}`)
   } catch (err) {
     console.error(err)
     res.send("Something went wrong.")
